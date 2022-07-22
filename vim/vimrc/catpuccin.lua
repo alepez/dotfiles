1818691a091bf3ef0952abd5e070e71739539c65
Alessandro Pezzato <alessandro.pezzato@gmail.com>
2022-07-22 11:10:57 +0200

    neovim: update catppuccin

--- vim/vimrc/catpuccin.lua
+++ vim/vimrc/catpuccin.lua
@@ -1,41 +1,69 @@
 local catppuccin = require("catppuccin")
 
 catppuccin.setup({
-  transparent_background = true,
+  dim_inactive = {
+    enabled = false,
+    shade = "dark",
+    percentage = 0.15,
+  },
+  transparent_background = false,
   term_colors = false,
+  compile = {
+    enabled = false,
+    path = vim.fn.stdpath "cache" .. "/catppuccin",
+  },
   styles = {
-    comments = "italic",
-    functions = "italic",
-    keywords = "italic",
-    strings = "NONE",
-    variables = "italic",
+    comments = { "italic" },
+    conditionals = { "italic" },
+    loops = {},
+    functions = {},
+    keywords = {},
+    strings = {},
+    variables = {},
+    numbers = {},
+    booleans = {},
+    properties = {},
+    types = {},
+    operators = {},
   },
   integrations = {
     treesitter = true,
     native_lsp = {
       enabled = true,
       virtual_text = {
-        errors = "italic",
-        hints = "italic",
-        warnings = "italic",
-        information = "italic",
+        errors = { "italic" },
+        hints = { "italic" },
+        warnings = { "italic" },
+        information = { "italic" },
       },
       underlines = {
-        errors = "underline",
-        hints = "underline",
-        warnings = "underline",
-        information = "underline",
+        errors = { "underline" },
+        hints = { "underline" },
+        warnings = { "underline" },
+        information = { "underline" },
       },
     },
+    coc_nvim = false,
     lsp_trouble = false,
     cmp = true,
     lsp_saga = false,
     gitgutter = false,
     gitsigns = true,
+    leap = false,
     telescope = true,
     nvimtree = {
       enabled = true,
-      show_root = false,
+      show_root = true,
+      transparent_panel = false,
+    },
+    neotree = {
+      enabled = false,
+      show_root = true,
+      transparent_panel = false,
+    },
+    dap = {
+      enabled = false,
+      enable_ui = false,
     },
     which_key = false,
     indent_blankline = {
@@ -54,7 +82,12 @@ catppuccin.setup({
     hop = false,
     notify = true,
     telekasten = true,
-  }
+    symbols_outline = true,
+    mini = false,
+    aerial = false,
+    vimwiki = true,
+    beacon = true,
+  },
 })
 
 
