f124ce3742bc7b8764e719f9fd93e3121f5f223b
Alessandro Pezzato <alessandro.pezzato@gmail.com>
2022-09-01 15:17:45 +0200

    Improve nvim-tree

--- vim/vimrc/nvim-tree.lua
+++ vim/vimrc/nvim-tree.lua
@@ -3,11 +3,10 @@ require'nvim-tree'.setup {
   hijack_netrw        = true,
   open_on_setup       = false,
   ignore_ft_on_setup  = {},
-  auto_close          = false,
   open_on_tab         = false,
   hijack_cursor       = false,
   update_cwd          = false,
-  update_to_buf_dir   = {
+  hijack_directories   = {
     enable = true,
     auto_open = true,
   },
@@ -43,7 +42,6 @@ require'nvim-tree'.setup {
     height = 30,
     hide_root_folder = false,
     side = 'left',
-    auto_resize = false,
     mappings = {
       custom_only = false,
       list = {}
