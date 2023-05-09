c9b67badd93932c520c0542c9d8612a85f6e3079
Alessandro Pezzato <alessandro.pezzato@gmail.com>
2023-05-09 18:26:22 +0200

    Fix lspconfig settings

--- home/.config/nvim/lua/extensions/mason.lua
+++ home/.config/nvim/lua/extensions/mason.lua
@@ -5,26 +5,91 @@ local lspconfig = require("lspconfig")
 mason.setup()
 mason_lspconfig.setup({
   ensure_installed = {
-    -- LSP for Lua language
-    "lua_ls",
-    -- LSP for Typescript and Javascript
-    "tsserver",
-    -- LSP for Emmet (Vue, HTML, CSS)
-    "emmet_ls",
-    -- LSP for CSS
+    "bashls",
+    "clangd",
     "cssls",
-    -- LSP for Docker
-    "dockerls",
-    -- LSP for Python
-    "pyright",
-    -- LSP for Vue
-    "volar"
+    "jedi_language_server",
+    -- "hls",
+    "html",
+    "rust_analyzer",
+    "lua_ls",
+    "taplo",
+    "tsserver",
+    "zls",
   }
 });
+
+local default_opts = {
+  flags = {
+    debounce_text_changes = 150,
+  }
+}
+
+local lua_runtime_path = vim.split(package.path, ';')
+table.insert(lua_runtime_path, "lua/?.lua")
+table.insert(lua_runtime_path, "lua/?/init.lua")
+
+local custom_opts = {
+  ["html"] = {
+    configurationSection = { "html", "css", "javascript" },
+    embeddedLanguages = {
+      css = true,
+      javascript = true
+    },
+    provideFormatter = true,
+  },
+  ["rust-analyzer"] = {
+    procMacro = {
+      enable = true
+    },
+    cargo = {
+      loadOutDirsFromCheck = true,
+    },
+    diagnostics = {
+      disabled = {
+        "unresolved-proc-macro"
+      }
+    }
+  },
+  ["lua_ls"] = {
+    cmd = {  "/usr/bin/lua-language-server" };
+    settings = {
+      Lua = {
+        runtime = {
+          version = 'LuaJIT',
+          path = lua_runtime_path,
+        },
+        diagnostics = {
+          -- Get the language server to recognize the `vim` global
+          globals = {'vim'},
+        },
+        workspace = {
+          -- Make the server aware of Neovim runtime files
+          library = vim.api.nvim_get_runtime_file("", true),
+          -- Avoid asking about working environment on every startup
+          checkThirdParty = false,
+        },
+        -- Do not send telemetry data containing a randomized but unique identifier
+        telemetry = {
+          enable = false,
+        },
+      },
+    },
+  },
+}
 
 -- Setup every needed language server in lspconfig
 mason_lspconfig.setup_handlers {
   function (server_name)
-    lspconfig[server_name].setup {}
+
+    local opts = custom_opts[server_name]
+
+    if opts == nil then
+      opts = default_opts
+    else
+      opts = vim.tbl_extend("keep", opts, default_opts)
+    end
+
+    lspconfig[server_name].setup(opts)
   end,
 }
--- home/.config/nvim/lua/keybindings.lua
+++ home/.config/nvim/lua/keybindings.lua
@@ -6,54 +6,76 @@ g.mapleader = ' '
 
 -- LSP, declaration
 nm('gD', '<Cmd>lua vim.lsp.buf.declaration()<CR>')
+
 -- LSP, definition
 nm('gd', '<Cmd>lua vim.lsp.buf.definition()<CR>')
+
 -- LSP, hover
 nm('<F1>', '<Cmd>lua vim.lsp.buf.hover()<CR>')
+
 -- LSP,  implementation
 nm('gi', '<cmd>lua vim.lsp.buf.implementation()<CR>')
+
 -- LSP, signature_help
 nm('<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>')
+
 -- LSP, type definition
 nm('gy', '<cmd>lua vim.lsp.buf.type_definition()<CR>')
+
 -- LSP, rename
 nm('<F2>', '<cmd>lua vim.lsp.buf.rename()<CR>')
+
 -- LSP, code action
 nm('<F5>', '<cmd>lua vim.lsp.buf.code_action()<CR>')
+
 -- LSP, references
 nm('gr', '<cmd>lua vim.lsp.buf.references()<CR>')
+
 -- LSP, diagnostic
 nm('<leader>e', '<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>')
+
 -- LSP, prev
 nm('[g', '<cmd>lua vim.diagnostic.goto_prev()<CR>')
+
 -- LSP, next
 nm(']g', '<cmd>lua vim.diagnostic.goto_next()<CR>')
+
 -- LSP, loclist
 nm('<leader>g', '<cmd>lua vim.diagnostic.setloclist()<CR>')
+
 -- LSP, auto format
 nm('<leader>af', "<cmd>lua vim.lsp.buf.format()<CR>")
 
 -- Telescope, Show recent files
 nm('tp', '<cmd>Telescope oldfiles<CR>')
+
 -- Telescope, Search for a file in project
 nm('to', '<cmd>Telescope git_files<CR>')
+
 -- Telescope, Show jumplist (previous locations)
 nm('<leader>j', '<cmd>Telescope jumplist<CR>')
+
 -- Telescope, Search for a file (ignoring git-ignore)
 nm('<C-p>', '<cmd>Telescope find_files<CR>')
+
 -- Telescope, Show git branches
 nm('tb', '<cmd>Telescope git_branches<CR>')
+
 -- Telescope, Find a string in project
 nm('<leader>F', '<cmd>Telescope live_grep<CR>')
+
 -- Telescope, Show all buffers
 nm('<leader>o', '<cmd>Telescope buffers<CR>')
+
 -- Telescope, Show all commands
 nm('<leader>;', '<cmd>Telescope<CR>')
+
 -- Telescope, Search for dynamic symbols
 nm('<leader>s', '<cmd>Telescope lsp_dynamic_workspace_symbols<CR>')
 
 -- Telescope, Show all problems in project (with help of LSP)
-nm('<leader>x', '<cmd>TroubleToggle<CR>')
+-- nm('<leader>x', '<cmd>TroubleToggle<CR>') TODO collision with buffer close
+
 -- Telescope, Show use of object in project
 nm('gr', '<cmd>Trouble lsp_references<CR>')
 
@@ -68,6 +90,9 @@ nm('<F6>', ':cn<CR>')
 
 -- Close buffer
 nm('<leader>x', ':bd<CR>')
+
+-- Save
+nm('<leader>w', ':w<CR>')
 
 -- Split line under cursor
 nm('K', 'i<CR><Esc>')
