633fe4dd7b3399e94433b89f3ec47e138af3d19b
Alessandro Pezzato <alessandro.pezzato@gmail.com>
2023-05-10 00:50:34 +0200

    Move plugins setup to lua files

--- home/.config/nvim/lua/plugins.lua
+++ home/.config/nvim/lua/plugins.lua
@@ -127,9 +127,13 @@ return {
 
   {
     "simrat39/rust-tools.nvim",
-    dependencies = { "neovim/nvim-lspconfig", "nvim-lua/plenary.nvim", "mfussenegger/nvim-dap" },
+    dependencies = {
+      "neovim/nvim-lspconfig",
+      "nvim-lua/plenary.nvim",
+      "mfussenegger/nvim-dap",
+    },
     opts = function(_, _)
-      require("rust-tools").setup({})
+      require "extensions.rust-tools"
     end,
   },
 
