05442930b45d7a767193c9ccc74017f1e7dff899
Alessandro Pezzato <alessandro.pezzato@gmail.com>
2023-05-09 23:14:36 +0200

    Disable git blame preview

--- home/.config/nvim/lua/extensions/gitsigns.lua
+++ home/.config/nvim/lua/extensions/gitsigns.lua
@@ -1,6 +1,6 @@
 local gitsigns = require('gitsigns')
 gitsigns.setup({
-  current_line_blame = true,
+  current_line_blame = false,
   current_line_blame_opts = {
     virt_text = true,
     virt_text_pos = 'right_align',
