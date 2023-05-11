f638d1fb37afc1eb73a5cf23082bb4d311dcea91
Alessandro Pezzato <alessandro.pezzato@gmail.com>
2023-05-11 23:35:16 +0200

    Clean up

--- home/.config/nvim-legacy/vimrc/lspconfig.lua
+++ home/.config/nvim-legacy/vimrc/lspconfig.lua
@@ -2,7 +2,7 @@ local lspconfig = require('lspconfig')
 
 -- Use an on_attach function to only map the following keys
 -- after the language server attaches to the current buffer
-local on_attach = function(client, bufnr)
+local on_attach = function(_, bufnr)
   local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
   local function buf_set_option(...) vim.api.nvim_buf_set_option(bufnr, ...) end
 
