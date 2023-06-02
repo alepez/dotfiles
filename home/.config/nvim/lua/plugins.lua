1613e3091b07a3662644b981e395103dad537769
Alessandro Pezzato <alessandro.pezzato@gmail.com>
2023-06-02 19:41:51 +0200

    Disable copilot

--- home/.config/nvim/lua/plugins.lua
+++ home/.config/nvim/lua/plugins.lua
@@ -183,23 +183,22 @@ return {
     }
   },
 
-  {
-    "zbirenbaum/copilot.lua",
-    config = function()
-      require("copilot").setup()
-    end
-  },
-
-  {
-    "zbirenbaum/copilot-cmp",
-    config = function()
-      require("copilot_cmp").setup({
-        suggestion = { enabled = false },
-        panel = { enabled = false },
-      }
-      )
-    end
-  },
+  -- {
+  --   "zbirenbaum/copilot.lua",
+  --   config = function()
+  --     require("copilot").setup({
+  --       suggestion = { enabled = false },
+  --       panel = { enabled = false },
+  --     })
+  --   end
+  -- },
+  --
+  -- {
+  --   "zbirenbaum/copilot-cmp",
+  --   config = function()
+  --     require("copilot_cmp").setup()
+  --   end
+  -- },
 
   {
     "iamcco/markdown-preview.nvim",
