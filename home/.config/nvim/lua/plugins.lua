b621c4fbd410defcd7425e833c6ca4029c38a5ad
Alessandro Pezzato <alessandro.pezzato@gmail.com>
2023-07-09 12:52:17 +0200

    neovim: enable copilot and update dependencies

--- home/.config/nvim/lua/plugins.lua
+++ home/.config/nvim/lua/plugins.lua
@@ -183,22 +183,22 @@ return {
     }
   },
 
-  -- {
-  --   "zbirenbaum/copilot.lua",
-  --   config = function()
-  --     require("copilot").setup({
-  --       suggestion = { enabled = false },
-  --       panel = { enabled = false },
-  --     })
-  --   end
-  -- },
-  --
-  -- {
-  --   "zbirenbaum/copilot-cmp",
-  --   config = function()
-  --     require("copilot_cmp").setup()
-  --   end
-  -- },
+  {
+    "zbirenbaum/copilot.lua",
+    config = function()
+      require("copilot").setup({
+        suggestion = { enabled = false },
+        panel = { enabled = false },
+      })
+    end
+  },
+
+  {
+    "zbirenbaum/copilot-cmp",
+    config = function()
+      require("copilot_cmp").setup()
+    end
+  },
 
   {
     "iamcco/markdown-preview.nvim",
