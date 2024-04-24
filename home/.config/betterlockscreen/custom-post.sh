4defbfd55c86adb237c683117267d93529355977
Alessandro Pezzato <alessandro.pezzato@gmail.com>
2024-04-24 08:08:55 +0200

    Fix screen standby after lock

--- home/.config/betterlockscreen/custom-post.sh
+++ home/.config/betterlockscreen/custom-post.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env bash
 
 # Note: this file is sourced by betterlockscreen, not executed
+
+# This is necessary to revert changes made by custom-pre.sh
+xset dpms "${DEFAULT_TIMEOUT}" "${DEFAULT_TIMEOUT}" "${DEFAULT_TIMEOUT}"
--- home/.config/betterlockscreen/custom-pre.sh
+++ home/.config/betterlockscreen/custom-pre.sh
@@ -1,3 +1,9 @@
 #!/usr/bin/env bash
 
 # Note: this file is sourced by betterlockscreen, not executed
+
+# Fix an issue with betterlockscreen, where the screen would not switch to
+# standby mode after locking. It is probably due to the fact that there are
+# three different timeout values for standby, suspend and off modes, and
+# betterlockscreen just overrides the first one.
+xset dpms "${lock_timeout}" "${lock_timeout}" "${lock_timeout}"
