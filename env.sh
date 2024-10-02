2e4f3eb9146aedc2aee58be2da289481dd2caaca
Alessandro Pezzato <alessandro.pezzato@standard.ai>
2024-10-02 14:46:20 +0200

    ssh-agent: add support for ssh agent, if gpg is not available

--- env.sh
+++ env.sh
@@ -29,13 +29,29 @@ if [ -z "${DOTFILES_ENV_SET}" ]; then
   fi
 
   # SSH Agent
-  # https://wiki.archlinux.org/title/GnuPG#Set_SSH_AUTH_SOCK
-  # Launch gpg-agent SSH_AUTH_SOCK only if is not set, to avoid conflicts with
-  # ssh-agent.
+  # Launch only if not already available (already started or forwarded)
   if [ -z "${SSH_AUTH_SOCK}" ]; then
-    unset SSH_AGENT_PID
-    if [ "${gnupg_SSH_AUTH_SOCK_by:-0}" -ne $$ ]; then
-      export SSH_AUTH_SOCK="$( gpgconf --list-dirs agent-ssh-socket )"
+    # Use gpg agent only if private keys are available and gpgconf is installed
+    # https://wiki.archlinux.org/title/GnuPG#Set_SSH_AUTH_SOCK
+    # Launch gpg-agent SSH_AUTH_SOCK only if is not set, to avoid conflicts with
+    # ssh-agent.
+    if which gpgconf >/dev/null && ls ${HOME}/.gnupg/private-keys-v1.d/*; then
+      unset SSH_AGENT_PID
+      if [ "${gnupg_SSH_AUTH_SOCK_by:-0}" -ne $$ ]; then
+        export SSH_AUTH_SOCK="$( gpgconf --list-dirs agent-ssh-socket )"
+      fi
+    elif which ssh-agent >/dev/null; then
+      # This will run a ssh-agent process if there is not one already, and save
+      # the output thereof. If there is one running already, we retrieve the
+      # cached ssh-agent output and evaluate it which will set the necessary
+      # environment variables.
+      # See https://wiki.archlinux.org/title/SSH_keys
+      if ! pgrep -u "$USER" ssh-agent > /dev/null; then
+          ssh-agent -t 1h > "$XDG_RUNTIME_DIR/ssh-agent.env"
+      fi
+      if [[ ! -f "$SSH_AUTH_SOCK" ]]; then
+          source "$XDG_RUNTIME_DIR/ssh-agent.env" >/dev/null
+      fi
     fi
   fi
 
