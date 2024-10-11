784f60a37686575a0d350ab4b5db03a68f49ac59
Alessandro Pezzato <alepez@users.noreply.github.com>
2024-10-11 17:21:12 +0200

    neovim: remove gen.nvim

--- home/.config/nvim/lua/plugins.lua
+++ home/.config/nvim/lua/plugins.lua
@@ -199,27 +199,6 @@ return {
     end,
   },
 
-  -- Custom Parameters (with defaults)
-  {
-    "David-Kunz/gen.nvim",
-    opts = {
-      model = "mistral", -- The default model to use.
-      display_mode = "float", -- The display mode. Can be "float" or "split".
-      show_prompt = true, -- Shows the Prompt submitted to Ollama.
-      show_model = true, -- Displays which model you are using at the beginning of your chat session.
-      no_auto_close = true, -- Never closes the window automatically.
-      init = function(options) pcall(io.popen, "ollama serve > /dev/null 2>&1 &") end,
-      -- Function to initialize Ollama
-      command = "curl --silent --no-buffer -X POST http://localhost:11434/api/generate -d $body",
-      -- The command for the Ollama service. You can use placeholders $prompt, $model and $body (shellescaped).
-      -- This can also be a lua function returning a command string, with options as the input parameter.
-      -- The executed command must return a JSON object with { response, context }
-      -- (context property is optional).
-      -- list_models = '<omitted lua function>', -- Retrieves a list of model names
-      debug = false -- Prints errors and the command which is run.
-    }
-  },
-
   {
     'mrcjkb/rustaceanvim',
     version = '^5',
