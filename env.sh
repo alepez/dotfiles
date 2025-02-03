8d4771a7bb53cd5370dfba07fe0ef4c2c51f4aed
Alessandro Pezzato <alepez@users.noreply.github.com>
2025-02-03 10:27:45 +0100

    Fix ssh agent

--- env.sh
+++ env.sh
@@ -28,33 +28,6 @@ if [ -z "${DOTFILES_ENV_SET}" ]; then
     source $HOME/.env.local
   fi
 
-  # SSH Agent
-  # Launch only if not already available (already started or forwarded)
-  if [ -z "${SSH_AUTH_SOCK}" ]; then
-    # Use gpg agent only if private keys are available and gpgconf is installed
-    # https://wiki.archlinux.org/title/GnuPG#Set_SSH_AUTH_SOCK
-    # Launch gpg-agent SSH_AUTH_SOCK only if is not set, to avoid conflicts with
-    # ssh-agent.
-    if which gpgconf >/dev/null && [ "$( ls ${HOME}/.gnupg/private-keys-v1.d/ | wc -l )" != 0 ]; then
-      unset SSH_AGENT_PID
-      if [ "${gnupg_SSH_AUTH_SOCK_by:-0}" -ne $$ ]; then
-        export SSH_AUTH_SOCK="$( gpgconf --list-dirs agent-ssh-socket )"
-      fi
-    elif which ssh-agent >/dev/null; then
-      # This will run a ssh-agent process if there is not one already, and save
-      # the output thereof. If there is one running already, we retrieve the
-      # cached ssh-agent output and evaluate it which will set the necessary
-      # environment variables.
-      # See https://wiki.archlinux.org/title/SSH_keys
-      if ! pgrep -u "$USER" ssh-agent > /dev/null; then
-          ssh-agent -t 1h > "$XDG_RUNTIME_DIR/ssh-agent.env"
-      fi
-      if [[ ! -f "$SSH_AUTH_SOCK" ]]; then
-          source "$XDG_RUNTIME_DIR/ssh-agent.env" >/dev/null
-      fi
-    fi
-  fi
-
   # Always use the adapter that uses the least possible power.
   # This is often an integrated GPU.
   # https://docs.rs/wgpu/latest/wgpu/
@@ -65,6 +38,8 @@ if [ -z "${DOTFILES_ENV_SET}" ]; then
   export PYTHON_KEYRING_BACKEND=keyring.backends.null.Keyring
 fi
 
-if [[ ! -f "$SSH_AUTH_SOCK" ]] && [ -e "$XDG_RUNTIME_DIR/ssh-agent.env" ]; then
-    source "$XDG_RUNTIME_DIR/ssh-agent.env" >/dev/null
+# This needs ssh-agent.service to be enabled
+# See https://wiki.archlinux.org/title/SSH_keys
+if [ -e "$XDG_RUNTIME_DIR/ssh-agent.socket" ]; then
+  export SSH_AUTH_SOCK="$XDG_RUNTIME_DIR/ssh-agent.socket"
 fi
