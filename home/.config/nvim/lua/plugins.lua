abec8d7fa6014772346f7fcf5dc12298ce881d24
Alessandro Pezzato <alepez@users.noreply.github.com>
2025-02-07 10:11:02 +0100

    neovim: remove easypick

--- home/.config/nvim/lua/plugins.lua
+++ home/.config/nvim/lua/plugins.lua
@@ -261,16 +261,6 @@ return {
   {
     "nvim-telescope/telescope-ui-select.nvim"
   },
-
-  {
-    "axkirillov/easypick.nvim",
-    dependencies = {
-      "nvim-telescope/telescope.nvim",
-    },
-    config = function()
-      require "extensions.easypick"
-    end,
-  },
 }
 
 -- vim:tabstop=2 shiftwidth=2 expandtab syntax=lua foldmethod=marker foldlevelstart=0 foldlevel=0
