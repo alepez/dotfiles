ee618df318f5b0f3c73ffe495082ab0396cd44d8
Alessandro Pezzato <877244+alepez@users.noreply.github.com>
2026-05-25 09:44:41 +0200

    clean up

---
+++ home/.config/hypr/monitors/radon-avatar.lua
@@ -0,0 +1,2 @@
+hl.monitor({ output = "desc:Lenovo Group Limited LEN D32q-20B 0x01010101", mode = "1920x1080", position = "auto", scale = 1 })
+hl.monitor({ output = "desc:XXX AAABox", mode = "1920x1080", position = "auto", scale = 1, mirror = "desc:Lenovo Group Limited LEN D32q-20B 0x01010101" })
